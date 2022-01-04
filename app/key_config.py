d6adae6ff5aaa7b630e60129c4a7c969f808aa2c
sabya.sachi <sabya.sachi@rackspace.com>
2022-01-04 15:10:39 +0530

    minor changes to access keys

--- app/key_config.py
+++ app/key_config.py
@@ -1,16 +1,14 @@
+
 # -*- coding: utf-8 -*-
 """
 Created on Thu Jan  01 19:56:55 2022
 @author: sachisabya
 """
 
-#fake Keys to access local dynamodb
-### todo
-#better ways to pull credenatils from dynamodb 
-
-ACCESS_KEY_ID='ASIA3FEP6WZKBLO6J3MQ'
-ACCESS_SECRET_KEY='/YSOI5dST5oC4fpIO5jc8299i23OoE6Y3rK4bheq'
-AWS_SESSION_TOKEN='FwoGZXIvYXdzEBUaDHiXR63f451C+fVW4CLJATOdeYYFp5Wj3GHPyDyUkF4xggM7jjOuP7X5+d9NiVTeTYm/kTk3444cgNzGtS6klfzYrkkkt0zT0ovFbOIonPcyl5gscA07Ov9ba8wZlhkNS4MkVItl55jWX0KTs0HBEMTfnMJ1k5/AeqwJYByZ5mYTSA4z6FVCwyeNpuuqkbidWs7TzYx/t46kmOWL6vRK7oqOdTNap7kCaXoBhWfw5AFKZyRO3T6QrPmbq1y4qPPqhCf2rOhnhFbZWMJnD8Bj5I+oIdWcoRBC3SiSwb34BTIt9P+BUrAdF1ovXYGrfg3Jc5pLGZdwC0jRH5l/ILtvYSo9hM73QClAz/6ZaWl7'
-
-
-
+'''
+fake Keys to access local dynamodb
+resource and boto3 client object access credentails from
+~/.aws/credentials
+'''
+# ACCESS_KEY_ID='AKIAWGXSLFZVWFN5DJJH'
+# ACCESS_SECRET_KEY='LEyP1wzmU6Bl219xmGXKpKRKEGe16xjlcEZuXa1f'
--- app/main.py
+++ app/main.py
@@ -1,11 +1,9 @@
-
-
 from flask import Flask, jsonify
 from app.config import app_config
 import boto3
 
 from app.routes import api as api_blueprint
-from app import dynamodb
+from app import dynamodb, dynamodb_client
 
 
 def create_app(config_name):   
@@ -16,7 +14,9 @@ def create_app(config_name):
     # app.config.from_pyfile('config.py')
     
     try:
-        if not dynamodb.Table('users'):
+        table_name = 'users'
+        existing_tables = dynamodb_client.list_tables()['TableNames']
+        if table_name not in existing_tables:
             table = dynamodb.create_table(
                 TableName='users',
                 KeySchema=[{'AttributeName': 'username','KeyType': 'HASH'}],
@@ -24,13 +24,11 @@ def create_app(config_name):
                 ProvisionedThroughput={'ReadCapacityUnits': 1,'WriteCapacityUnits': 1})    
             
             # Wait until the table exists.
-            table.meta.client.get_waiter('table_exists').wait(TableName='userdata')
+            table.meta.client.get_waiter('table_exists').wait(TableName='users')
             # Print out some data about the table.
-            print('table Created, table count : ',table.item_count)      
+            print('table Created, table : ',table)      
     except Exception as ex:
         return jsonify({'message': 'table not created'})
 
     app.register_blueprint(api_blueprint)
     return app
-
-
